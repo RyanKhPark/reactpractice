30c3cdf03cfb8f85121eadb8b080f4a39b38f9c2
RyanKhPark <53807537+RyanKhPark@users.noreply.github.com>
2023-03-04 07:01:20 +0900

    initial commit~

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      breeds: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://dog.ceo/api/breeds/list/all')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => this.setState(() => {
+        const strData = data.message
+        return { breeds: strData }
+      })
+      )
+  }
+
+  // tricky stuff: render and show a page with each Dog breed type and for each breed type have a bullt point and names. Even this, if the breed type does not have sub type dont show the breed type.
+
+  //--
+
+  render() {
+    const breeds = this.state.breeds
+    // for (const [key, value] of Object.entries(breeds)) {
+    //   console.log(`${key} and ${value}`)
+    // }
+    return (
+      <div className="App">
+
+        "Hello World!"
+        {Object.entries(breeds).map((breed) => { return console.log(breed[1]) })}
+        {Object.entries(breeds).map((breed) => {
+          return breed[1].length > 0 ?
+            <div>
+              <h2 key={breed[0]}>{breed[0]}</h2>
+              {breed[1].map((subBreed) => { return <li>{subBreed}</li> })}
+            </div> : ''
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
